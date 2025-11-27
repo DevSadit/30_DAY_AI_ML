05a6d140f52715d22766be98d644953ecbc95f20
DevSadit <dev.sadit@gmail.com>
2025-11-27 13:23:23 +0600

    function learning

---
+++ Day_1/function.py
@@ -0,0 +1,45 @@
+def my_py_fn():
+    print("kire mama, kmn asos!")
+
+
+# my_py_fn()
+
+
+# using return
+def _hello():
+    return "hello"
+
+
+message = _hello()
+# print(message)
+
+
+# pass statement
+# it is used to make a function without defining it.
+def kola_kadir():
+    pass
+
+
+# function arguments
+# to pass the information to the function
+# def my_function(name, age):  # name is a parameter
+# print("Hello", name, " you are ", age, " years old")
+
+
+# name = "Cihiro"
+# old = 20
+# my_function(name, old)  # "Emil" is an argument
+
+
+# declare function
+def amr_fn(name, age, work):
+    print(f"amar name {name} and i am {age} years old. lastly i am a {work}")
+
+
+# call the function
+
+amar_name = "sadit"
+amr_boyos = 20
+amr_kaj = "student"
+
+amr_fn(amar_name, amr_boyos, amr_kaj)
