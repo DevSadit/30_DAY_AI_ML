6b37c982d067f3ffd2152c77bf29c38ad670b0e1
DevSadit <dev.sadit@gmail.com>
2025-11-27 12:17:17 +0600

    basic file created

---
+++ Day_1/basics.py
@@ -0,0 +1,55 @@
+# print("hello world")
+
+# age = 20
+# print(age)
+
+# print(type(age))
+
+
+# name = input("please enter your name: ")
+# print("Hello, " + name)
+
+# logical operator(and): both condition need to be true
+
+# x = 10
+# if x > 5 and x < 11:
+#     print("x is valid")
+# else:
+#     print("x is not valid")
+
+
+# logical operator(or): one condition need to be true
+
+# x = 10
+# if x > 5 or x < 9:
+#     print("x is valid")
+# else:
+#     print("x is not valid")
+
+
+# logical operator(not):
+
+# x = 10
+# if x != 5:
+#     print("tor x tor kase e rak!")
+
+
+# type casting: which change the type of the data
+# they are:
+# 1. int()
+# 2. float()
+# 3. str()
+
+# x = "kola"
+# y = 10
+
+# y_prime = str(y)
+# print(type(y_prime))
+
+
+# F-strings
+# name = "sadit"
+# age = 20
+# work = "student"
+# print(f"my name is {name} and im {age} years old")
+# print(f"im a {work}")
