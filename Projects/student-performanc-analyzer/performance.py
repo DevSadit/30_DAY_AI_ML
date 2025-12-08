e76c5ddcfcb4e0cde073eb29b8678ac7b395b1a4
DevSadit <dev.sadit@gmail.com>
2025-12-08 21:32:48 +0600

    matplotlib basic recap

--- Projects/student-performanc-analyzer/performance.py
+++ Projects/student-performanc-analyzer/performance.py
@@ -1,21 +1,21 @@
-# Load the dataset
-# Find each student’s average score
-# Find the top 3 performers
-# Find subject-wise highest & lowest score
-# Make a bar chart showing each student’s average
-# Create a pass/fail column (pass = avg > 60)
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("students.csv")
 # print(df)
 
-# average for all students
+# average collumn
 df["average"] = df[["math", "science", "english"]].mean(axis=1)
-# print(df)
+# print(df["average"])
 
 
-# top 3 performers
-top_3 = df["average"].sort_values("average", ascending=False)
-print(top_3)
+# top 3 performance
+top_3 = df.sort_values("average", ascending=False).head(3)
+# print(top_3)
+
+
+# subject wise highest and lowest
+highest_math = df.loc[df["math"].idxmax()]
+lowest_math = df.loc[df["math"].idxmin()]
+
+print(highest_math, lowest_math)
